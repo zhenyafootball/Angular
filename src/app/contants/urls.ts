528a19c7789f3c708adf24728a2b596a46114e49
zhenyafootball <fiatscudo2@gmail.com>
2022-05-17 17:00:06 +0300

    yes

---
+++ src/app/contants/urls.ts
@@ -0,0 +1,10 @@
+import {environment} from '../../environments/environment';
+
+
+const {API} = environment;
+
+export const urls = {
+  users: `${API}/users`,
+  posts: `${API}/posts`,
+  comments: `${API}/comments`,
+}
